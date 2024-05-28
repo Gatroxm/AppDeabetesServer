6634206e802fc09033619e05da66fbc499400340
Gustavo Adolfo Muñoz Reyes <157825951+Desarrollo8Tuatara@users.noreply.github.com>
2024-05-28 18:35:51 -0500

    Ajuste de prueba

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,2 @@
 module.exports.SEED = '@este-es@-#el-$$seed-@@de-_-Q@@desarrollo';
+module.exports.PORT = 'https://gatroxm.github.io/AppDeabetesServer'
--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json())
 //Cros
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'https://gatroxm.github.io/AppDeabetesServer');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
